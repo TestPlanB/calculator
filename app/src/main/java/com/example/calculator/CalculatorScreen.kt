d58d44dd650bd33b741ccaa2694f0507f61a0037
TestPlanB <1395828939@qq.com>
2022-08-24 17:52:13 +0800

    reset

--- app/src/main/java/com/example/calculator/CalculatorScreen.kt
+++ app/src/main/java/com/example/calculator/CalculatorScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -23,7 +24,9 @@ fun Screen(sum: String) {
         modifier = Modifier
             .fillMaxWidth()
     ) {
-        val (text, img) = createRefs()
+        val (text, img) = remember {
+            createRefs()
+        }
         Image(
             painter = painterResource(R.mipmap.pika_screen),
             contentScale = ContentScale.FillWidth,
@@ -37,11 +40,15 @@ fun Screen(sum: String) {
                     end.linkTo(parent.end)
                 }
         )
-        Text(text = sum, textAlign = TextAlign.Right, fontSize = 30.sp, modifier = Modifier.constrainAs(text) {
-            top.linkTo(parent.top)
-            bottom.linkTo(parent.bottom)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        })
+        Text(
+            text = sum,
+            textAlign = TextAlign.Right,
+            fontSize = 30.sp,
+            modifier = Modifier.constrainAs(text) {
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            })
     }
 }
